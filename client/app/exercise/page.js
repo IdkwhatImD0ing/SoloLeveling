0601a287969f41221325e56c78639c6af39d84e8
Restyled.io <commits@restyled.io>
2024-02-10 23:39:58 +0000

    Restyled by clang-format

--- client/app/exercise/page.js
+++ client/app/exercise/page.js
@@ -1,18 +1,18 @@
 "use client"
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 
 export default function Exercise() {
   const videoRef = useRef(null);
 
   useEffect(() => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(stream => {
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        })
-        .catch(err => console.error(err));
+      navigator.mediaDevices.getUserMedia({video : true})
+          .then(stream => {
+            if (videoRef.current) {
+              videoRef.current.srcObject = stream;
+            }
+          })
+          .catch(err => console.error(err));
     }
   }, []);
 
