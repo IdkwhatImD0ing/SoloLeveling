c02ce1e9fe9b36ca2ef663736b96ea74f3e46bd0
Restyled.io <commits@restyled.io>
2024-02-10 23:39:59 +0000

    Restyled by prettier

--- client/app/exercise/page.js
+++ client/app/exercise/page.js
@@ -1,25 +1,28 @@
-"use client"
-import React, {useEffect, useRef} from 'react';
+"use client";
+import React, { useEffect, useRef } from "react";
 
 export default function Exercise() {
   const videoRef = useRef(null);
 
   useEffect(() => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({video : true})
-          .then(stream => {
-            if (videoRef.current) {
-              videoRef.current.srcObject = stream;
-            }
-          })
-          .catch(err => console.error(err));
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
+        })
+        .catch((err) => console.error(err));
     }
   }, []);
 
   return (
     <main>
       <h1>Exercise</h1>
-      <p>Get started by editing <code>app/exercise.js</code></p>
+      <p>
+        Get started by editing <code>app/exercise.js</code>
+      </p>
       <video ref={videoRef} autoPlay />
     </main>
   );
