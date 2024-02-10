30940f4aa2a74d2dc142fda4c7a264f01e2cf69a
Restyled.io <commits@restyled.io>
2024-02-10 22:50:16 +0000

    Restyled by prettier

--- client/app/exercise/page.js
+++ client/app/exercise/page.js
@@ -1,12 +1,10 @@
-
-
 export default function Exercise() {
-
-    return (
-        <main>
-            <h1>Exercise</h1>
-            <p>Get started by editing <code>app/exercise.js</code></p>
-        </main>
-    );
-
+  return (
+    <main>
+      <h1>Exercise</h1>
+      <p>
+        Get started by editing <code>app/exercise.js</code>
+      </p>
+    </main>
+  );
 }
