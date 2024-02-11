f8e01ba32eb8bff25a1e672f3b48569427f7e1d8
Restyled.io <commits@restyled.io>
2024-02-11 13:55:29 +0000

    Restyled by clang-format

--- client/app/navbar/page.js
+++ client/app/navbar/page.js
@@ -1,26 +1,34 @@
-import React from "react";
-import styles from "../globals.scss";
-import styles1 from "./styles.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPersonRunning, faChartColumn, faUserGroup, faGamepad, faGear, faArrowRight, faFire } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRight,
+  faChartColumn,
+  faFire,
+  faGamepad,
+  faGear,
+  faPersonRunning,
+  faUserGroup
+} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Link from 'next/link';
+import React from "react";
+
+import styles from "../globals.scss";
+
+import styles1 from "./styles.scss";
 
 const NavBar = () => {
-    return (
-        <nav>
-            <Link href="/exercise">
-                <FontAwesomeIcon icon={faPersonRunning} className='nav-icons' />
-            </Link>
-            <Link href="/dashboard">
-                <FontAwesomeIcon icon={faChartColumn} className='nav-icons' />
-            </Link>
+  return (
+      <nav><Link href = "/exercise">
+      <FontAwesomeIcon icon = {faPersonRunning} className = 'nav-icons' />
+      </Link>
+            <Link href="/dashboard ">
+           < FontAwesomeIcon icon = {faChartColumn} className = 'nav-icons' />
+      </Link>
             <FontAwesomeIcon icon={faUserGroup} className='nav-icons' />
-            <FontAwesomeIcon icon={faGamepad} className='nav-icons' />
-            <Link href="/">
-                <FontAwesomeIcon icon={faGear} className='nav-icons' />
-            </Link>
-        </nav>
-    );
+      <FontAwesomeIcon icon = {faGamepad} className = 'nav-icons' />
+      <Link href = "/">
+      <FontAwesomeIcon icon = {faGear} className = 'nav-icons' />
+      </Link>
+        </nav>);
 };
 
 export default NavBar;
--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,16 @@
-import { UserButton, ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from "@clerk/nextjs";
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Link from 'next/link';
+
 import styles from "./globals.scss";
 import styles1 from "./home.scss"
-import Link from 'next/link';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Landing() {
 
@@ -25,9 +32,10 @@ export default function Landing() {
 
         {/* <div className="h-screen">
           <UserButton afterSignOutUrl="/"/>
-        </div> */}
+        </div> */
+}
 
-        {/* <video
+{/* <video
           src="https://cdn.vidzflow.com/v/H0LDXZjsnq_1080p_1703000142.mp4"
           autoPlay
           loop
@@ -37,20 +45,19 @@ export default function Landing() {
         <div className="hero__video--lighting"></div>
         <div className="hero__inner">
           {/* <Nav /> */}
-          {/* <div className="divider"></div> */}
+          {/* <div className="divider"></div> */
+}
 
-          <div className="hero__below-divider">
-            <div className="home__hero--headline">
-              <div className="home__hero-1">
-                <div>What if working out wasn't just a chore...</div>
-              </div>
-              {/* <div className="home__hero-2">
-                <div>...</div>
-              </div>
-              <div className="home__hero-3">
-                <div>Working out wasn't just a chore</div>
-              </div> */}
-              <div className="home__hero-4">
+<div className = "hero__below-divider"><div className = "home__hero--headline">
+    <div className = "home__hero-1">
+    <div>What if working out wasn't just a chore...</div> <
+    /div>
+              {/*<div className = "home__hero-2">
+    <div>...</div>
+              </div><div className = "home__hero-3">
+    <div>Working out wasn't just a chore</div> < /div> */
+}
+<div className="home__hero-4">
                 <div>But a game?</div>
               </div>
             </div>
@@ -66,21 +73,19 @@ export default function Landing() {
           <div className="call-action">
             <SignedIn>
               <Link className='start' href="/dashboard">
-                  Let's Start
-                  <FontAwesomeIcon icon={faArrowRight} />
-              </Link>            
-            </SignedIn>
+Let's Start <FontAwesomeIcon icon = {faArrowRight} />
+              </Link><
+    /SignedIn>
             <SignedOut>
-              <Link className='start' href="/sign-in">
-                  Make an Account or Log in
-                  <FontAwesomeIcon icon={faArrowRight} />
-              </Link>            
-            </SignedOut>            
+              <Link className='start' href="/sign - in
+    ">
+Make an Account or Log in
+    <FontAwesomeIcon icon = {faArrowRight} />
+              </Link>
+    </SignedOut>            
           </div>
 
-
-        </div>
-      </section>
     </div>
+      </section>< /div>
   );
 };
