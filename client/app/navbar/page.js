9ed44bfbc05f21889acfb326e88290d3af382a9c
Restyled.io <commits@restyled.io>
2024-02-11 13:55:31 +0000

    Restyled by prettier

--- client/app/navbar/page.js
+++ client/app/navbar/page.js
@@ -5,10 +5,10 @@ import {
   faGamepad,
   faGear,
   faPersonRunning,
-  faUserGroup
+  faUserGroup,
 } from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import Link from 'next/link';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 import React from "react";
 
 import styles from "../globals.scss";
@@ -17,18 +17,20 @@ import styles1 from "./styles.scss";
 
 const NavBar = () => {
   return (
-      <nav><Link href = "/exercise">
-      <FontAwesomeIcon icon = {faPersonRunning} className = 'nav-icons' />
+    <nav>
+      <Link href="/exercise">
+        <FontAwesomeIcon icon={faPersonRunning} className="nav-icons" />
       </Link>
-            <Link href="/dashboard ">
-           < FontAwesomeIcon icon = {faChartColumn} className = 'nav-icons' />
+      <Link href="/dashboard ">
+        <FontAwesomeIcon icon={faChartColumn} className="nav-icons" />
       </Link>
-            <FontAwesomeIcon icon={faUserGroup} className='nav-icons' />
-      <FontAwesomeIcon icon = {faGamepad} className = 'nav-icons' />
-      <Link href = "/">
-      <FontAwesomeIcon icon = {faGear} className = 'nav-icons' />
+      <FontAwesomeIcon icon={faUserGroup} className="nav-icons" />
+      <FontAwesomeIcon icon={faGamepad} className="nav-icons" />
+      <Link href="/">
+        <FontAwesomeIcon icon={faGear} className="nav-icons" />
       </Link>
-        </nav>);
+    </nav>
+  );
 };
 
 export default NavBar;
