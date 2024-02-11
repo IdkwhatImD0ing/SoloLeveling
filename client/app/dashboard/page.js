d6f261faca1ecd8b00cd10a3d118711b196dfa68
Hebe3834 <hebeluna00@gmail.com>
2024-02-10 19:23:04 -0500

    changed from css to scss

---
+++ client/app/dashboard/page.js
@@ -0,0 +1,14 @@
+"use client"
+import React, { useEffect, useRef } from 'react';
+import styles from "../globals.scss";
+import styles from "./styles.scss";
+import Image from "next/image";
+
+export default function Dashboard() {
+    return (
+        <div className=' dashboard'>
+            <div className='purple-blob1'></div>
+            <div className='purple-blob2'></div>
+        </div>
+    );
+}
--- client/app/layout.js
+++ client/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
