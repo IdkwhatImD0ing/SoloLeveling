f9b7088e56121521294cdf4342757872245a5420
Hebe3834 <hebeluna00@gmail.com>
2024-02-11 06:03:08 -0500

    more landing

--- client/app/page.js
+++ client/app/page.js
@@ -1,4 +1,4 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import styles from "./globals.scss";
 import styles1 from "./home.scss"
 import Link from 'next/link';
@@ -8,49 +8,73 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function Landing() {
 
   return (
-    
-    <section id="hero">
-      <div className="h-screen">
-        <UserButton afterSignOutUrl="/"/>
-      </div>
-      {/* <video
-        src="https://cdn.vidzflow.com/v/H0LDXZjsnq_1080p_1703000142.mp4"
-        autoPlay
-        loop
-        muted
-      /> */}
-
-      <div className="hero__video--lighting"></div>
-      <div className="hero__inner">
-        {/* <Nav /> */}
-        <div className="divider"></div>
-        <div className="hero__below-divider">
-          <div className="home__hero--headline">
-            <div className="home__hero-1">
-              <div>What if</div>
-            </div>
-            <div className="home__hero-2">
-              <div>...</div>
-            </div>
-            <div className="home__hero-3">
-              <div>Working out wasn't just a chore</div>
-            </div>
-            <div className="home__hero-4">
-              <div className="overflow__none">But a game?</div>
-            </div>
+    <div>
+      <nav>
+        <SignedOut>
+          <Link className="signin" href="/sign-in">Sign In</Link>
+          <Link className="signup" href="/sign-up">Sign Up</Link>
+        </SignedOut>
+        <SignedIn>
+          <div className="h-screen">
+            <UserButton afterSignOutUrl="/"/>
           </div>
-          <h1 className="hero__h1">
-            Introducing Questify, the app that transforms your workout routine into an exciting game, motivating you to level up your fitness with each exercise session
-          </h1>
-          {/* <div className="hero__search--outer">
-            <Searchbar search = {search}  setSearch = {setSearch} nav_funct={() => navigate('/bills')}/>
-          </div> */}
+        </SignedIn>
+      </nav>
+    
+      <section id="hero">
+
+        {/* <div className="h-screen">
+          <UserButton afterSignOutUrl="/"/>
+        </div> */}
+
+        {/* <video
+          src="https://cdn.vidzflow.com/v/H0LDXZjsnq_1080p_1703000142.mp4"
+          autoPlay
+          loop
+          muted
+        /> */}
+
+        <div className="hero__video--lighting"></div>
+        <div className="hero__inner">
+          {/* <Nav /> */}
+          <div className="divider"></div>
+          <div className="hero__below-divider">
+            <div className="home__hero--headline">
+              <div className="home__hero-1">
+                <div>What if</div>
+              </div>
+              <div className="home__hero-2">
+                <div>...</div>
+              </div>
+              <div className="home__hero-3">
+                <div>Working out wasn't just a chore</div>
+              </div>
+              <div className="home__hero-4">
+                <div>But a game?</div>
+              </div>
+            </div>
+            <h1 className="hero__h1">
+              Introducing Questify, the app that transforms your workout routine into an exciting game, motivating you to level up your fitness with each exercise session
+            </h1>
+            {/* <div className="hero__search--outer">
+              <Searchbar search = {search}  setSearch = {setSearch} nav_funct={() => navigate('/bills')}/>
+            </div> */}
+          </div>
+          <SignedIn>
+            <Link className='start' href="/dashboard">
+                Let's Start
+                <FontAwesomeIcon icon={faArrowRight} />
+            </Link>            
+          </SignedIn>
+          <SignedOut>
+            <Link className='start' href="/sign-in">
+                Make an Account / Log in
+                <FontAwesomeIcon icon={faArrowRight} />
+            </Link>            
+          </SignedOut>
+
         </div>
-        <Link className='start' href="/dashboard">
-            Let's Start
-            <FontAwesomeIcon icon={faArrowRight} />
-        </Link>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
