34251a781bfa4eb9d8bdfc1d0c5bea69357ae7ab
Jay Wu <jayqw@uci.edu>
2024-02-11 00:42:30 -0800

    add clerk functionality

---
+++ client/app/sign-up/page.js
@@ -0,0 +1,18 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  // Inline styles for the container
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    width: '100vw', 
+  };
+
+  return (
+    <div style={containerStyle}>
+      <SignUp />
+    </div>
+  );
+}
