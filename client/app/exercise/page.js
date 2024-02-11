96c70ad9ebcd5a38dcc483fe44b73cc9f9d2fdec
Restyled.io <commits@restyled.io>
2024-02-11 00:32:37 +0000

    Restyled by clang-format

--- client/app/exercise/page.js
+++ client/app/exercise/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 import styles from './exercise.css';
 
 export default function Exercise() {
@@ -7,31 +7,27 @@ export default function Exercise() {
 
   useEffect(() => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(stream => {
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        })
-        .catch(err => console.error(err));
+      navigator.mediaDevices.getUserMedia({video : true})
+          .then(stream => {
+            if (videoRef.current) {
+              videoRef.current.srcObject = stream;
+            }
+          })
+          .catch(err => console.error(err));
     }
   }, []);
 
-  return (
-    <main>
-      <div className={styles.container}> 
-        <div className='video__container'>
-          <video ref={videoRef} autoPlay playsInline muted></video>
-        </div>
-        <div className='quests__container'>
-          <div className='daily__container'>
+  return (<main><div className = {styles.container}>
+          <div className = 'video__container'>
+          <video ref = {videoRef} autoPlay playsInline muted>
+          </video>
+        </div><div className = 'quests__container'>
+          <div className = 'daily__container'>
           </div>
           <div className='weekly__container'>
           </div>
-          <div className='event__container'>
-          </div>
+          <div className = 'event__container'></div>
         </div>
-      </div>
-    </main>
-  );
+          </div>
+    </main>);
 }
