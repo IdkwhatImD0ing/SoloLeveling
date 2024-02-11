90c3f77652cf3a88e08a6d2c485edb701d04346d
Restyled.io <commits@restyled.io>
2024-02-11 00:32:41 +0000

    Restyled by prettier

--- client/app/exercise/page.js
+++ client/app/exercise/page.js
@@ -1,33 +1,35 @@
-"use client"
-import React, {useEffect, useRef} from 'react';
-import styles from './exercise.css';
+"use client";
+import React, { useEffect, useRef } from "react";
+import styles from "./exercise.css";
 
 export default function Exercise() {
   const videoRef = useRef(null);
 
   useEffect(() => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({video : true})
-          .then(stream => {
-            if (videoRef.current) {
-              videoRef.current.srcObject = stream;
-            }
-          })
-          .catch(err => console.error(err));
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
+        })
+        .catch((err) => console.error(err));
     }
   }, []);
 
-  return (<main><div className = {styles.container}>
-          <div className = 'video__container'>
-          <video ref = {videoRef} autoPlay playsInline muted>
-          </video>
-        </div><div className = 'quests__container'>
-          <div className = 'daily__container'>
-          </div>
-          <div className='weekly__container'>
-          </div>
-          <div className = 'event__container'></div>
+  return (
+    <main>
+      <div className={styles.container}>
+        <div className="video__container">
+          <video ref={videoRef} autoPlay playsInline muted></video>
         </div>
-          </div>
-    </main>);
+        <div className="quests__container">
+          <div className="daily__container"></div>
+          <div className="weekly__container"></div>
+          <div className="event__container"></div>
+        </div>
+      </div>
+    </main>
+  );
 }
