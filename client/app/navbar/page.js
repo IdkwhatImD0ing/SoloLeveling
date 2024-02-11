77a1c46e43f3cb3d943488ea5bad5a75b6fd604d
Restyled.io <commits@restyled.io>
2024-02-11 12:02:48 +0000

    Restyled by prettier

--- client/app/navbar/page.js
+++ client/app/navbar/page.js
@@ -5,9 +5,9 @@ import {
   faGamepad,
   faGear,
   faPersonRunning,
-  faUserGroup
+  faUserGroup,
 } from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 import styles from "../globals.scss";
@@ -15,15 +15,15 @@ import styles from "../globals.scss";
 import styles1 from "./styles.scss";
 
 const NavBar = () => {
-    return (
-        <nav>
-            <FontAwesomeIcon icon={faPersonRunning} className='nav-icons' />
-            <FontAwesomeIcon icon={faChartColumn} className='nav-icons' />
-            <FontAwesomeIcon icon={faUserGroup} className='nav-icons' />
-            <FontAwesomeIcon icon={faGamepad} className='nav-icons' />
-            <FontAwesomeIcon icon={faGear} className='nav-icons' />
-        </nav>
-    );
+  return (
+    <nav>
+      <FontAwesomeIcon icon={faPersonRunning} className="nav-icons" />
+      <FontAwesomeIcon icon={faChartColumn} className="nav-icons" />
+      <FontAwesomeIcon icon={faUserGroup} className="nav-icons" />
+      <FontAwesomeIcon icon={faGamepad} className="nav-icons" />
+      <FontAwesomeIcon icon={faGear} className="nav-icons" />
+    </nav>
+  );
 };
 
 export default NavBar;
