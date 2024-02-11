445b0b71089e0a59cc1931c741b6f3a3ca42adeb
Jay Wu <jayqw@uci.edu>
2024-02-11 04:36:58 -0800

    initially call daily quests

--- client/app/page.js
+++ client/app/page.js
@@ -1,28 +1,54 @@
-import { UserButton, ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+"use client";
+import io from "socket.io-client";
+import {
+  UserButton,
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 import styles from "./globals.scss";
-import styles1 from "./home.scss"
-import Link from 'next/link';
+import styles1 from "./home.scss";
+import Link from "next/link";
+import React, { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Landing() {
+  const socket = useRef(null);
+  useEffect(() => {
+    // Initialize Socket.IO connection
+    socket.current = io("http://localhost:8000");
+
+    socket.current.on("connect", () => {
+      console.log("Socket.IO connected");
+    });
+    socket.current.emit("get_daily_quest", process.env.NEXT_PUBLIC_DEMO_EMAIL);
+    // Cleanup on component unmount
+    return () => {
+      if (socket.current) socket.current.disconnect();
+    };
+  }, []);
 
   return (
     <div>
       <nav>
         <SignedOut>
-          <Link className="signin" href="/sign-in">Sign In</Link>
-          <Link className="signup" href="/sign-up">Sign Up</Link>
+          <Link className="signin" href="/sign-in">
+            Sign In
+          </Link>
+          <Link className="signup" href="/sign-up">
+            Sign Up
+          </Link>
         </SignedOut>
         <SignedIn>
           <div className="h-screen">
-            <UserButton afterSignOutUrl="/"/>
+            <UserButton afterSignOutUrl="/" />
           </div>
         </SignedIn>
       </nav>
-    
-      <section id="hero">
 
+      <section id="hero">
         {/* <div className="h-screen">
           <UserButton afterSignOutUrl="/"/>
         </div> */}
@@ -54,27 +80,28 @@ export default function Landing() {
               </div>
             </div>
             <h1 className="hero__h1">
-              Introducing Questify, the app that transforms your workout routine into an exciting game, motivating you to level up your fitness with each exercise session
+              Introducing Questify, the app that transforms your workout routine
+              into an exciting game, motivating you to level up your fitness
+              with each exercise session
             </h1>
             {/* <div className="hero__search--outer">
               <Searchbar search = {search}  setSearch = {setSearch} nav_funct={() => navigate('/bills')}/>
             </div> */}
           </div>
           <SignedIn>
-            <Link className='start' href="/dashboard">
-                Let's Start
-                <FontAwesomeIcon icon={faArrowRight} />
-            </Link>            
+            <Link className="start" href="/dashboard">
+              Let's Start
+              <FontAwesomeIcon icon={faArrowRight} />
+            </Link>
           </SignedIn>
           <SignedOut>
-            <Link className='start' href="/sign-in">
-                Make an Account / Log in
-                <FontAwesomeIcon icon={faArrowRight} />
-            </Link>            
+            <Link className="start" href="/sign-in">
+              Make an Account / Log in
+              <FontAwesomeIcon icon={faArrowRight} />
+            </Link>
           </SignedOut>
-
         </div>
       </section>
     </div>
   );
-};
+}
